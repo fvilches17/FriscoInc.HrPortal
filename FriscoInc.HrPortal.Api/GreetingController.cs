9a7d4238f86473c9b9b6a28f6274662c57f50887
Francisco Vilches <f_vilches@outlook.com>
2021-08-18 00:07:19 +1200

    add install .net 6 step in pipeline

--- FriscoInc.HrPortal.Api/GreetingController.cs
+++ FriscoInc.HrPortal.Api/GreetingController.cs
@@ -15,7 +15,7 @@ namespace FriscoInc.HrPortal.Api
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "greeting")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
